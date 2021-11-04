6678019864d6f223d24e96ddb98410c40faed49b
anna <milanna888@mail.ru>
2021-11-04 22:59:59 +0300

    update with login links

--- entry.php
+++ entry.php
@@ -1,10 +1,15 @@
 <?php
-
-
+session_start();
 ?>
+
 <div class="sidebar">
     <h2>Вход</h2>
+    <?php 
+    if($_SESSION['user']){?>
+      <div class="btn_entry"><a href="lk.php">Личный кабинет</a></div>
+      <?php }else{?>
       <div class="btn_entry"><a href="avto.php">Вход</a></div>
       <br>
       <div class="btn_entry"><a href="register.php">Регистрация</a></div>
+      <?php }?>
 </div>
