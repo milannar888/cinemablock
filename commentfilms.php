cd3176a80e83df53af693888069a84bc45ec7d5a
anna <milanna888@mail.ru>
2021-11-08 07:34:04 +0300

    getting started with a comment

---
+++ commentfilms.php
@@ -0,0 +1,7 @@
+<div class="send">
+  <form id="review" action="#" method="POST">
+    <input name="review_name" type="text" placeholder="ваше имя">
+    <textarea name="review_text"></textarea>
+    <input class="btn" type="submit" value="отправить">
+  </form>
+</div>
--- show_palma.php
+++ show_palma.php
@@ -77,13 +77,9 @@ session_start();
         </div>
         </div>
         <!-- Форма оставить отзыв -->
-      <div class="send">
-        <form id="review" action="#" method="POST">
-          <input name="review_name" type="text" placeholder="ваше имя">
-          <textarea name="review_text"></textarea>
-          <input class="btn" type="submit" value="отправить">
-        </form>
-      </div>
+        <?php
+          include "commentfilms.php";
+        ?> 
     </div>
   </div>
 <!-- Подвал сайта -->
